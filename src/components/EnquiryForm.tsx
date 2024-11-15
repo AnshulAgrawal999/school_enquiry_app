66f6fd2b7dc42a060dc50ca56ad889c899178c56
anshul <anshulagr799@gmail.com>
2024-11-15 12:39:54 +0530

    Date of Birth Added

--- src/components/EnquiryForm.tsx
+++ src/components/EnquiryForm.tsx
@@ -15,7 +15,7 @@ type EnquiryFormData = {
     guardianPhoneNumber : string  ;
     guardianMobileNumberOpt : string  ;
     studentName: string  ;
-    dateOfBirth: string  ;
+    dateOfBirth: Date  ;
     class: string  ;
     currentSchool: string  ;
     address: {
@@ -45,12 +45,44 @@ const DevTool: React.ComponentType<DevToolProps> = dynamic(
 
 export default function EnquiryForm() {
 
-  const form = useForm<EnquiryFormData>()  ;
+  const form = useForm<EnquiryFormData>(
+    {
+      defaultValues : {
+        guardianName : ""  ,
+        relation : ""  ,
+        guardianEmail : ""  ,
+        guardianPhoneNumber : ""  ,
+        guardianMobileNumberOpt : ""  ,
+        studentName: ""  ,
+        class: "1st"  ,
+        currentSchool: ""  ,
+        address: {
+          street: ""  ,
+          city: ""  ,
+          state: ""  ,
+          pinCode: ""  ,
+          country: "India"  ,
+        }  ,
+        enquirySource: ""  ,
+        description: ""  ,
+        wantHostel: false  
+      }
+    }
+  )  ;
 
-  const { register , control , handleSubmit , formState: { errors }, reset } = form  ;  
+  const { register , control , handleSubmit , formState , reset } = form  ;  
+
+  const { errors , isSubmitting } = formState  ;
 
   const onSubmit = ( data : EnquiryFormData ) => {
+
     console.log( 'form submitted' , data )  ;
+
+    reset()  ;
+  }
+
+  const handleResetForm = ( ) => {
+    reset()  ;  
   }
 
   return (
@@ -65,9 +97,15 @@ export default function EnquiryForm() {
           <h2> Guardian Information Section </h2>
 
           <div className="enqFields" >
+
             <div>
               <label htmlFor="guardianName"> Guardian Name </label>
-              <input type="text" id="guardianName" { ...register( 'guardianName' , { required : 'Guardian Name is required'  } )  } />
+              <input type="text" id="guardianName" { ...register( 'guardianName' , { required : { 
+                  value : true ,
+                  message : 'Guardian Name is required'
+                }  } )  } />
+
+              <p> { errors.guardianName?.message } </p>
             </div>
 
             <div>
@@ -78,19 +116,29 @@ export default function EnquiryForm() {
               <input
                 type="text"
                 id="relation"
-                { ...register( 'relation' , { required : 'Relation is required'  } ) }
+                { ...register( 'relation' , { required :  { 
+                  value : true ,
+                  message : 'Relation is required'
+                }  } ) }
               />
+
+              <p> { errors.relation?.message } </p>
             </div>
 
             <div>
               <label htmlFor="guardianEmail" > Guardian Email </label>
               <input type="email" id="guardianEmail" { ...register( 'guardianEmail' , { 
-                required: "Guardian email is required",
+                required: { 
+                  value : true ,
+                  message : 'Guardian email is required'
+                },
                 pattern : { 
                   value : /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i ,
                   message : "Please Enter a valid email"
                 }
                } ) } />
+
+               <p> { errors.guardianEmail?.message } </p>
             </div>
 
             <div>
@@ -102,18 +150,23 @@ export default function EnquiryForm() {
                 type="number"
                 id="guardianPhoneNumber"
                 maxLength={10}
-                { ...register( 'guardianPhoneNumber', { required : 'Guardian Phone is required' ,
+                { ...register( 'guardianPhoneNumber', { required : { 
+                  value : true ,
+                  message : 'Guardian Phone is required'
+                } ,
                   pattern: {
-                    value: /^[0-9]*$/,
+                    value: /^[0-9]{10}$/,
                     message: "Please enter a valid phone number",
                   }, } ) }
               />
+
+              <p> { errors.guardianPhoneNumber?.message } </p>
             </div>
 
             <div>
               <label htmlFor="guardianMobileNumberOpt">
                 {" "}
-                Guardian Mobile Number{" "}
+                Guardian Mobile Number (Optional) {" "}
               </label>
               <input
                 type="number"
@@ -121,15 +174,38 @@ export default function EnquiryForm() {
                 maxLength={10}
                 { ...register( 'guardianMobileNumberOpt' , {
                   pattern: {
-                    value: /^[0-9]*$/,
+                    value: /^[0-9]{10}$/,
                     message: "Please enter a valid mobile number",
                   },
                 } ) }
               />
+
+              <p> { errors.guardianMobileNumberOpt?.message } </p>
+            </div>
+
+          </div>
+          
+          <h2> Student Information Section </h2>
+          
+          <div className="enqFields" >
+            
+            <div>
+
+              <label htmlFor="dateOfBirth"> Date Of Birth </label>
+              <input type="date" id="dateOfBirth" { ...register( 'dateOfBirth' , { 
+                  valueAsDate : true ,
+                  required : { 
+                  value : true ,
+                  message : 'Date Of Birth is required'
+                }  } )  } />
+
+              <p> { errors.dateOfBirth?.message } </p>
+            
             </div>
           </div>
+                <button type="submit"> submit form </button>
                 
-                <button> submit form </button>
+                <button type="submit" onClick={handleResetForm} > reset form </button>
 
         </form>
         
